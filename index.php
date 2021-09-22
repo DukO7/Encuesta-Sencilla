5e0cc8adb8e924f9041005a827b38b2382b9d7d2
DukO7 <89145197+DukO7@users.noreply.github.com>
2021-09-21 19:17:59 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Encuesta Estudio-Socio Economico</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+
+
+<body bgcolor="white">
+    <h1>Estudio-Socio Economico </h1>
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color="black">1. Actualmente vives con: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color="black"> 1) Tus padres.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color="black"> 2) En pension.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="c">
+    <font color="black"> 3) Con familiares.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="d">
+    <font color="black"> 4) Otros.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+</form>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color="black">2. Tu familia se compone por: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color="black"> 1) Padres y hermanos.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color="black"> 2) Padre o Madre.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="c">
+    <font color="black"> 3) Otros.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+</form>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color="black">3. Tu padre vive: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color="black"> 1) Si.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color="black"> 2) No.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+</form>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color="black">4. Tu madre vive: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color="black"> 1) Si.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color="black"> 2) No.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+</form>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color="black">5. Grado de escolaridad del padre: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color="black"> 1) Nivel basico.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color="black"> 2) Nivel medio.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="c">
+    <font color="black"> 3) Nivel superior.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="d">
+    <font color="black"> 4) Ninguno.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+</form>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color="black">6. Grado de escolaridad de la madre: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color="black"> 1) Nivel basico.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color="black"> 2) Nivel medio.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="c">
+    <font color="black"> 3) Nivel superior.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="d">
+    <font color="black"> 4) Ninguno.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+</form>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>7. Eres hijo unico: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1) Si.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2) No.</font></font></p>
+  <p> <font color="#FFFFFF">
+    </font></p>
+</form>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>8. Numero de integrantes en la familia: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1)  3 Integrantes.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2)  4 Integrantes.</font></font></p>
+  <p> <font color="#FFFFFF">
+     <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>3)  5 Integrantes.</font></font></p>
+  <p> <font color="#FFFFFF">
+     <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>4)  6 Integrantes.</font></font></p>
+  <p> <font color="#FFFFFF">
+     <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>5)  Mas de 6 integrantes.</font></font></p>
+  <p> <font color="#FFFFFF">
+</form>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>9. En tu opinion, tus padres (padre o madre) te otorgan una atencion: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1) Muy buena.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2) Buena. </font></font></p>
+  <p> <font color="#FFFFFF">
+    <input type="radio" name="op" value="b">
+    <font color=black>3) Regular.</font></font></p>
+  <p> <font color="#FFFFFF">
+   
+    </font></p>
+</form>
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>10. En tu opinion, las condiciones economicas para tus estudios: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1) Buenas. </font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2) Regulares.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>3) Malas.</font></font></p>
+    </font></p>
+</form>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>11. Como consideras el ambiente familiar entre cada uno de los integrantes de tu familia: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1) Excelente. </font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2) Bueno.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>3) Regular.</font></font></p>
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>4) Pesimo.</font></font></p>
+    </font></p>
+</form>
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>12 . Desde tu punto de vista, cual es la persona en la que confias para contarle tus problemas personales: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1) Padre. </font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2) Madre.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>3) Hermano(a).</font></font></p>
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>4) Familiar cercano.</font></font></p>
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>5) Amistad.</font></font></p>
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>6) Nadie.</font></font></p>
+    </font></p>
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>13. La responsabilidad economicade tu familia recae en: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1) Padre. </font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2) Madre.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>3) Ambos.</font></font></p>
+    </font></p>
+    <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>4) Otro familiar.</font></font></p>
+    </font></p>
+</form>
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>14. Trabajas: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1) No. </font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2) Si.</font></font></p>
+  <input type="text" name="nombreUsuario" placeholder="Nombre de la empres:"  required > 
+  <input type="text" name="nombreUsuario" placeholder="Direccion:"  required > 
+  <input type="text" name="nombreUsuario" placeholder="Telefono:"  required > 
+    
+</form>
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>15. Escuela de procedencia: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <input type="text" name="nombreUsuario" placeholder="Escuela:"  required > 
+</form>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>16. Consideras que tu desempeño en la escuela de procedencia fue:</font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1) Muy bueno. </font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2) Bueno.</font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>3) Regular.</font></font></p>
+    </font></p>
+</form>
+
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>17. En la escuela de procedencia asistias de manera regular a clases: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1) Si. </font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2) No.</font></font></p>
+</form>
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>18. Tienes algun problema de tipo fisico que pudiese provocar un menor rendimiento en alguna materia: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1) No. </font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2) Si.</font></font></p>
+    <input type="text" name="nombreUsuario" placeholder="Cual:"  required > 
+</form>
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color=black>19. Has tenido difilcultades: </font></strong></font></p>
+<form name="form1" method="post" action="encuesta.php">
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color=black>1) No. </font></font></p>
+  <p> <font color="#FFFFFF" size="2" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color=black>2) Si.</font></font></p>
+    <input type="text" name="nombreUsuario" placeholder="Cual:"  required > 
+</form>
+</form>
+ <input type="submit" name="Submit" value="Enviar">
+<p>&nbsp;</p>
+</body>
+</html>
